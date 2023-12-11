167c55b432e13c5939306d13ea52c2d63678def1
dEIOnTomsoN <125672061+dEIOnTomsoN@users.noreply.github.com>
2023-12-11 13:18:53 +0530

    Update ee.c

--- ee.c
+++ ee.c
@@ -1,165 +1,188 @@
-#include <stdio.h>
-#include <stdlib.h>
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
 
-// Define the structure for a polynomial term
-struct Term {
-    int coefficient; // Coefficient
-    int exponent;    // Exponent
-    struct Term* next;
-};
+class Game implements Serializable {
+    String gameName;
+    double price;
+    char forp;
+    double disc;
 
-// Function prototypes
-struct Term* create_term(int coefficient, int exponent);
-void add_term(struct Term** poly, int coefficient, int exponent);
-void display_poly(const char* label, const struct Term* poly);
-struct Term* multiply_polynomials(const struct Term* poly1, const struct Term* poly2);
-struct Term* add_polynomials(const struct Term* poly1, const struct Term* poly2);
-void get_input_poly(struct Term** poly);
-void free_poly(struct Term* poly);
-
-int main() {
-    struct Term* poly1 = NULL;
-    struct Term* poly2 = NULL;
-    struct Term* product_result = NULL;
-    struct Term* sum_result = NULL;
-
-    // Input for the first polynomial
-    printf("Enter the first polynomial:\n");
-    get_input_poly(&poly1);
-
-    // Input for the second polynomial
-    printf("Enter the second polynomial:\n");
-    get_input_poly(&poly2);
-
-    // Display the first and second polynomials
-    display_poly("1st Polynomial", poly1);
-    display_poly("2nd Polynomial", poly2);
-
-    // (a) Multiply the polynomials
-    product_result = multiply_polynomials(poly1, poly2);
-    printf("Product of Polynomials: ");
-    display_poly("Result", product_result);
-
-    // (b) Add the polynomials
-    sum_result = add_polynomials(poly1, poly2);
-    printf("Sum of Polynomials: \n");
-    display_poly("1st Polynomial", poly1);
-    display_poly("2nd Polynomial", poly2);
-    display_poly("Result", sum_result);
-
-    // Manual memory deallocation
-    free_poly(poly1);
-    free_poly(poly2);
-    free_poly(product_result);
-    free_poly(sum_result);
-
-    return 0;
-}
-
-// Function to create a new term
-struct Term* create_term(int coefficient, int exponent) {
-    struct Term* newTerm = (struct Term*)malloc(sizeof(struct Term));
-    if (newTerm == NULL) {
-        perror("Memory allocation error");
-        exit(EXIT_FAILURE);
+    Game(String gameName, double price, char forp, double disc) {
+        this.gameName = gameName;
+        this.price = price;
+        this.forp = forp;
+        this.disc = disc;
     }
-    newTerm->coefficient = coefficient;
-    newTerm->exponent = exponent;
-    newTerm->next = NULL;
-    return newTerm;
+
+    void display() {
+        // Implementation of display method
+    }
 }
 
-// Function to add a term to the polynomial
-void add_term(struct Term** poly, int coefficient, int exponent) {
-    struct Term* newTerm = create_term(coefficient, exponent);
-    if (*poly == NULL) {
-        *poly = newTerm;
-    } else {
-        struct Term* temp = *poly;
-        while (temp->next != NULL) {
-            temp = temp->next;
+public class GameStoreGUI extends JFrame {
+    private final ArrayList<Game> games;
+
+    public GameStoreGUI() {
+        games = new ArrayList<>();
+        setTitle("Game Store");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+
+        createMainMenu();
+    }
+
+    private void createMainMenu() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JButton insertButton = new JButton("Insert Game");
+        JButton viewButton = new JButton("View a Game");
+        JButton viewAllButton = new JButton("View All Games");
+        JButton updateButton = new JButton("Update Discount");
+        JButton exitButton = new JButton("Exit");
+
+        insertButton.addActionListener(e -> showInsertWindow());
+        viewButton.addActionListener(e -> showViewWindow());
+        viewAllButton.addActionListener(e -> showViewAllWindow());
+        updateButton.addActionListener(e -> showUpdateWindow());
+        exitButton.addActionListener(e -> System.exit(0));
+
+        panel.add(insertButton);
+        panel.add(viewButton);
+        panel.add(viewAllButton);
+        panel.add(updateButton);
+        panel.add(exitButton);
+
+        add(panel);
+    }
+
+    private void showInsertWindow() {
+        JFrame insertFrame = new JFrame("Insert Game");
+        insertFrame.setSize(300, 200);
+        insertFrame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new GridLayout(5, 2));
+
+        JTextField gameNameField = new JTextField();
+        JTextField priceField = new JTextField();
+        JTextField forpField = new JTextField();
+        JTextField discField = new JTextField();
+
+        panel.add(new JLabel("Game Name:"));
+        panel.add(gameNameField);
+        panel.add(new JLabel("Price:"));
+        panel.add(priceField);
+        panel.add(new JLabel("Free or Paid (F/P):"));
+        panel.add(forpField);
+        panel.add(new JLabel("Discount Percentage:"));
+        panel.add(discField);
+
+        JButton insertButton = new JButton("Insert");
+        insertButton.addActionListener(e -> {
+            try {
+                String gameName = gameNameField.getText();
+                double price = Double.parseDouble(priceField.getText());
+                char forp = forpField.getText().toUpperCase().charAt(0);
+                double disc = Double.parseDouble(discField.getText());
+
+                Game newGame = new Game(gameName, price, forp, disc);
+                games.add(newGame);
+
+                JOptionPane.showMessageDialog(insertFrame, "Game inserted successfully!");
+                insertFrame.dispose();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(insertFrame, "Invalid input. Please enter valid values.");
+            }
+        });
+
+        panel.add(insertButton);
+
+        insertFrame.add(panel);
+        insertFrame.setVisible(true);
+    }
+
+    private void showViewWindow() {
+        JFrame viewFrame = new JFrame("View a Game");
+        viewFrame.setSize(300, 100);
+        viewFrame.setLocationRelativeTo(null);
+
+        String gameName = JOptionPane.showInputDialog(viewFrame, "Enter the Game Name:");
+
+        if (gameName != null) {
+            for (Game game : games) {
+                if (game.gameName.equalsIgnoreCase(gameName)) {
+                    game.display();
+                    viewFrame.dispose();
+                    return;
+                }
+            }
+
+            JOptionPane.showMessageDialog(viewFrame, "Game not found!");
         }
-        temp->next = newTerm;
+
+        viewFrame.dispose();
     }
-}
 
-// Function to display the polynomial
-void display_poly(const char* label, const struct Term* poly) {
-    printf("%s: ", label);
-    while (poly != NULL) {
-        printf("%dx^%d", poly->coefficient, poly->exponent);
-        poly = poly->next;
-        if (poly != NULL) {
-            printf(" + ");
-        }
-    }
-    printf("\n");
-}
+    private void showViewAllWindow() {
+        JFrame viewAllFrame = new JFrame("View All Games");
+        viewAllFrame.setSize(400, 300);
+        viewAllFrame.setLocationRelativeTo(null);
 
-// Function to multiply two polynomials
-struct Term* multiply_polynomials(const struct Term* poly1, const struct Term* poly2) {
-    struct Term* result = NULL;
-    struct Term* temp = poly2;
+        JTextArea outputArea = new JTextArea();
 
-    while (poly1 != NULL) {
-        while (temp != NULL) {
-            int coeff = poly1->coefficient * temp->coefficient;
-            int exp = poly1->exponent + temp->exponent;
-
-            add_term(&result, coeff, exp);
-            temp = temp->next;
+        for (Game game : games) {
+            outputArea.append("Game Name: " + game.gameName + "\n");
+            outputArea.append("Price: Rs." + game.price + "\n");
+            outputArea.append("Free or Paid: " + (game.forp == 'F' ? "Free" : "Paid") + "\n");
+            outputArea.append("Discount Percentage: " + game.disc + "%\n");
+            double discountedPrice = game.price - (game.price * game.disc / 100);
+            outputArea.append("Price after Discount: Rs." + discountedPrice + "\n\n");
         }
 
-        poly1 = poly1->next;
-        temp = poly2;
+        JScrollPane scrollPane = new JScrollPane(outputArea);
+        viewAllFrame.add(scrollPane);
+
+        viewAllFrame.setVisible(true);
     }
 
-    return result;
-}
+    private void showUpdateWindow() {
+        JFrame updateFrame = new JFrame("Update Discount");
+        updateFrame.setSize(300, 150);
+        updateFrame.setLocationRelativeTo(null);
 
-// Function to add two polynomials
-struct Term* add_polynomials(const struct Term* poly1, const struct Term* poly2) {
-    struct Term* result = NULL;
-    while (poly1 != NULL || poly2 != NULL) {
-        int coeff1 = (poly1 != NULL) ? poly1->coefficient : 0;
-        int exp1 = (poly1 != NULL) ? poly1->exponent : 0;
+        String gameName = JOptionPane.showInputDialog(updateFrame, "Enter the Game Name to update:");
+        if (gameName != null) {
+            String newDiscountStr = JOptionPane.showInputDialog(updateFrame, "Enter the new Discount Percentage:");
+            if (newDiscountStr != null) {
+                try {
+                    double newDiscount = Double.parseDouble(newDiscountStr);
 
-        int coeff2 = (poly2 != NULL) ? poly2->coefficient : 0;
-        int exp2 = (poly2 != NULL) ? poly2->exponent : 0;
+                    for (Game game : games) {
+                        if (game.gameName.equalsIgnoreCase(gameName)) {
+                            game.disc = newDiscount;
 
-        int sum_coeff = coeff1 + coeff2;
+                            JOptionPane.showMessageDialog(updateFrame, "Discount updated successfully!");
+                            updateFrame.dispose();
+                            return;
+                        }
+                    }
 
-        add_term(&result, sum_coeff, exp1);
+                    JOptionPane.showMessageDialog(updateFrame, "Game not found!");
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(updateFrame, "Invalid input. Please enter a valid discount.");
+                }
+            }
+        }
 
-        if (poly1 != NULL) poly1 = poly1->next;
-        if (poly2 != NULL) poly2 = poly2->next;
+        updateFrame.dispose();
     }
-    return result;
-}
 
-// Function to get input polynomial from the user
-void get_input_poly(struct Term** poly) {
-    int num_terms;
-    int coefficient, exponent;
-
-    printf("Enter the number of terms in the polynomial: ");
-    scanf("%d", &num_terms);
-
-    for (int i = 0; i < num_terms; ++i) {
-        printf("Enter coefficient for term %d: ", i + 1);
-        scanf("%d", &coefficient);
-        printf("Enter exponent for term %d: ", i + 1);
-        scanf("%d", &exponent);
-
-        add_term(poly, coefficient, exponent);
-    }
-}
-
-// Function to free memory used by the polynomial
-void free_poly(struct Term* poly) {
-    while (poly != NULL) {
-        struct Term* temp = poly;
-        poly = poly->next;
-        free(temp);
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GameStoreGUI().setVisible(true));
     }
 }
